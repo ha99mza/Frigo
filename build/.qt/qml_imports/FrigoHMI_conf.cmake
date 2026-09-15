d860c744597436db2175e5432c646b898c6983da
Hamza <h.louaddi@gmail.com>
2026-09-15 10:58:07 +0100

    Update Version QT 6.2

--- build/.qt/qml_imports/FrigoHMI_conf.cmake
+++ build/.qt/qml_imports/FrigoHMI_conf.cmake
@@ -1,7 +1,7 @@
 set(qml_import_scanner_imports_count 30)
 set(qml_import_scanner_import_0 "CLASSNAME;QtQuick2Plugin;LINKTARGET;Qt6::qtquick2plugin;NAME;QtQuick;PATH;C:/Qt/6.11.2/mingw_64/qml/QtQuick;PLUGIN;qtquick2plugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/;RELATIVEPATH;QtQuick;TYPE;module;")
 set(qml_import_scanner_import_1 "CLASSNAME;QtQmlPlugin;LINKTARGET;Qt6::qmlplugin;NAME;QtQml;PATH;C:/Qt/6.11.2/mingw_64/qml/QtQml;PLUGIN;qmlplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/;RELATIVEPATH;QtQml;TYPE;module;")
-set(qml_import_scanner_import_2 "NAME;QML;PATH;C:/Users/hlwad/Desktop/Frigo/build/FrigoHMI/QML;PREFER;:/qt/qml/FrigoHMI/;RELATIVEPATH;QML;TYPE;module;")
+set(qml_import_scanner_import_2 "NAME;QML;PATH;C:/Users/hlwad/Desktop/Frigo/build/FrigoHMI/QML;PREFER;:/FrigoHMI/;RELATIVEPATH;QML;TYPE;module;")
 set(qml_import_scanner_import_3 "CLASSNAME;QtQmlModelsPlugin;LINKTARGET;Qt6::modelsplugin;NAME;QtQml.Models;PATH;C:/Qt/6.11.2/mingw_64/qml/QtQml/Models;PLUGIN;modelsplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/Models/;RELATIVEPATH;QtQml/Models;TYPE;module;")
 set(qml_import_scanner_import_4 "CLASSNAME;QtQmlWorkerScriptPlugin;LINKTARGET;Qt6::workerscriptplugin;NAME;QtQml.WorkerScript;PATH;C:/Qt/6.11.2/mingw_64/qml/QtQml/WorkerScript;PLUGIN;workerscriptplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQml/WorkerScript/;RELATIVEPATH;QtQml/WorkerScript;TYPE;module;")
 set(qml_import_scanner_import_5 "CLASSNAME;QtQuickLayoutsPlugin;LINKTARGET;Qt6::qquicklayoutsplugin;NAME;QtQuick.Layouts;PATH;C:/Qt/6.11.2/mingw_64/qml/QtQuick/Layouts;PLUGIN;qquicklayoutsplugin;PLUGINISOPTIONAL;;PREFER;:/qt-project.org/imports/QtQuick/Layouts/;RELATIVEPATH;QtQuick/Layouts;TYPE;module;")
